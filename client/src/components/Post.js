3145325f3b07ccd9692091e7813a80df0f002658
vinodselpol <vinodselpol@gmail.com>
2024-03-24 23:52:55 -0500

    post fixes

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, TextField, Typography, IconButton, Paper, Avatar, Stack, Switch, Grid } from '@mui/material';
+import { Box, Button, TextField, Typography, IconButton, Paper, Avatar, Stack, Switch, Grid, Modal } from '@mui/material';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import CommentIcon from '@mui/icons-material/Comment';
 import FaceIcon from '@mui/icons-material/Face';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import moment from 'moment';
+import CloseIcon from '@mui/icons-material/Close';
+
+function generateGradient(post) {
+  // Simple hash function to get a number from post ID
+  const hash = post.split('').reduce((acc, char) => char.charCodeAt(0) + ((acc << 5) - acc), 0);
+  const startColor = `hsl(${hash % 360}, 70%, 80%)`;
+  const endColor = `hsl(${(hash * 2) % 360}, 70%, 60%)`;
+
+  return `linear-gradient(135deg, ${startColor}, ${endColor})`;
+}
 
 
 function Post({ post, postId, updatePosts, onDelete, isModerator }) {
@@ -13,9 +23,9 @@ function Post({ post, postId, updatePosts, onDelete, isModerator }) {
   const [showComments, setShowComments] = useState(false);
   const [autoCommentEnabled, setAutoCommentEnabled] = useState(false);
   const [generatedComment, setGeneratedComment] = useState('');
-  const [postBgColor, setPostBgColor] = useState('');
+  const [openModal, setOpenModal] = useState(false);
 
-  const colors = ['#FFCDD2', '#F8BBD0', '#E1BEE7', '#D1C4E9', '#C5CAE9', '#BBDEFB', '#B3E5FC', '#B2EBF2', '#B2DFDB', '#C8E6C9', '#DCEDC8', '#F0F4C3', '#FFF9C4', '#FFECB3', '#FFE0B2', '#FFCCBC', '#D7CCC8', '#CFD8DC'];
+ 
 
   const handleNewCommentChange = (event) => setNewComment(event.target.value);
 
@@ -25,15 +35,6 @@ function Post({ post, postId, updatePosts, onDelete, isModerator }) {
       setNewComment(''); // Clear the input field when auto-generate is disabled
     }
   };
-
-  const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
-
-  // Set a random background color for the post when it mounts
-  useEffect(() => {
-    setPostBgColor(getRandomColor());
-  }, []);
-
-
 
   const fetchGeneratedComment = async () => {
     try {
@@ -59,10 +60,6 @@ function Post({ post, postId, updatePosts, onDelete, isModerator }) {
     }
   }, [autoCommentEnabled]);
 
-
- 
-  
-
   const submitComment = async () => {
     const commentToSubmit = newComment.trim() || generatedComment.trim();
     if (commentToSubmit) {
@@ -82,21 +79,27 @@ function Post({ post, postId, updatePosts, onDelete, isModerator }) {
       setAutoCommentEnabled(false);
     }
   }
+  const backgroundStyle = { backgroundImage: generateGradient(post._id.toString()) };
 
   return (
-<Paper elevation={2} sx={{ 
-  my: 2, 
-  borderRadius: 3, 
-  overflow: 'hidden', 
-  border: '1px solid #e0e0e0', 
-  display: 'flex', 
-  flexDirection: 'column', 
-  height: '100%', // Set the height of each post container
-}}>
+    <Paper elevation={2} sx={{ 
+      my: 2, 
+      borderRadius: 3, 
+      overflow: 'hidden', 
+      border: '1px solid #e0e0e0', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      height: '100%',
+      transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out', // Smooth transition for transform and shadow
+      ':hover': {
+        transform: 'scale(1.03)', // Slightly scale up the post
+        boxShadow: '0 4px 20px rgba(0,0,0,0.2)', // Enhanced shadow effect on hover
+      },
+    }}>
   <Box
     sx={{
       height: 200,
-      backgroundColor: postBgColor, // This would be replaced with an actual image later
+      backgroundColor: backgroundStyle, // This would be replaced with an actual image later
     }}
   />
   {/* Content Area with Fixed Max Height and Scrollbar */}
@@ -156,78 +159,6 @@ function Post({ post, postId, updatePosts, onDelete, isModerator }) {
           {showComments ? 'Hide Comments' : 'Show Comments'}
         </Button>
       </Box>
-  {/* {showComments && (
-  <Box mt={3} px={3} pb={2}>
-    {post.comments.length ? (
-      post.comments.map((comment, index) => (
-        <Paper
-          key={index}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            p: 2,
-            mb: 2,
-            bgcolor: 'background.paper',
-            borderRadius: 2,
-            boxShadow: '0 1px 3px rgba(0,0,0,0.1)'
-          }}
-        >
-          <Avatar sx={{ bgcolor: deepPurple[500], mr: 2 }}>
-            {comment.userName.charAt(0).toUpperCase()}
-          </Avatar>
-          <Box flexGrow={1}>
-            <Typography variant="body2" fontWeight="bold" color="primary.main">
-              {comment.userName.toUpperCase()}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {comment.text}
-            </Typography>
-          </Box>
-        </Paper>
-      ))
-    ) : (
-      <Typography color="text.secondary">No comments yet.</Typography>
-    )}
-    <TextField
-      fullWidth
-      variant="filled"
-      label="Add a comment"
-      value={newComment}
-      onChange={handleNewCommentChange}
-      margin="normal"
-      sx={{ my: 2, '.MuiFilledInput-root': { bgcolor: 'background.paper' } }}
-    />
-    <Box mt={2} display="flex" justifyContent="space-between" alignItems="center">
-      <Button
-        onClick={submitComment}
-        variant="contained"
-        color="primary"
-        sx={{ textTransform: 'none', borderRadius: 2, height: '40px' }}
-      >
-        Submit Comment
-      </Button>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          p: '4px',
-          borderRadius: '8px',
-          border: '1px solid #e0e0e0',
-          backgroundColor: '#f0f0f0',
-          color: '#333'
-        }}
-      >
-        <Typography sx={{ fontWeight: 'bold', fontSize: '0.875rem', mr: 1 }}>
-          AI Comment:
-        </Typography>
-        <Switch
-          checked={autoCommentEnabled}
-          onChange={(e) => handleAutoCommentToggle(e.target.checked)}
-        />
-      </Box>
-    </Box>
-  </Box>
-)} */}
 {showComments && (
   <Box mt={3} px={3} pb={2} sx={{ maxHeight: 300, overflow: 'auto' }}>
     {post.comments.length ? (
